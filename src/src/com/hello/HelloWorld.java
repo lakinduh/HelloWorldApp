8a26cd7ec65f41885209d78da2517a91e12d640e
Lakindu Herath <lakindu@ittex.com.au>
2013-02-01 11:25:54 +0530

    HelloWorldApp created

---
+++ src/src/com/hello/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.hello;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: lakindu
+ * Date: 2/1/13
+ * Time: 10:59 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+      public static void main(String args[])
+      {
+          System.out.println("GitHub test project");
+      }
+}
